6ab4f0c0b11b4f2fb4446ecca556ab714b18e957
YourBunnyWrote0000 <57093177+YourBunnyWrote0000@users.noreply.github.com>
2021-05-12 15:27:25 +0300

    Initialization PostgreSQL

--- protecteddata/protecteddata/settings.py
+++ protecteddata/protecteddata/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'protecteddata.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ProtectedData',
+        'USER': 'admin',
+        'PASSWORD' : '123qweasdzxc',
+        'HOST' : 'localhost',
+        'PORT': '5432'
     }
 }
 
