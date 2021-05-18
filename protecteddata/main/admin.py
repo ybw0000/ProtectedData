ecc5d850a567c45e5d86baa81b0aec6f29f30144
YourBunnyWrote0000 <57093177+YourBunnyWrote0000@users.noreply.github.com>
2021-05-18 18:18:10 +0300

    Functional
    Added fucntional for "Send invite" form

--- protecteddata/main/admin.py
+++ protecteddata/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Contact
+from .models import Contact, Invite
 
 admin.site.register(Contact)
+admin.site.register(Invite)
--- protecteddata/main/models.py
+++ protecteddata/main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Contact(models.Model):
     name = models.CharField('Name', max_length=50)
-    email = models.CharField('Email', max_length=50)
+    email = models.EmailField('Email', max_length=50)
     phone = models.CharField('Phone number', max_length=20)
     message = models.TextField('Text message')
 
@@ -12,3 +12,13 @@ class Contact(models.Model):
     class Meta:
         verbose_name = 'Contact'
         verbose_name_plural = 'Contacts'
+
+class Invite(models.Model):
+    email = models.EmailField('Email', max_length=50)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Invite'
+        verbose_name_plural = 'Invites'
