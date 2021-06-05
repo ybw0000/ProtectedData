4fade74f357dd0078872aed53052a59d153f4ebc
NasadykVlad <52100186+NasadykVlad@users.noreply.github.com>
2021-06-05 17:51:29 +0300

    Update login and signup button's

--- protecteddata/protecteddata/settings.py
+++ protecteddata/protecteddata/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'protecteddata.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'ProtectedData',
-       'USER': 'admin',
-       'PASSWORD' : '123qweasdzxc',
-       'HOST' : 'localhost',
-       'PORT': '5432'
-   }
-}
+#DATABASES = {
+ #  'default': {
+  #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+  #     'NAME': 'ProtectedData',
+  #     'USER': 'admin',
+  #     'PASSWORD' : '123qweasdzxc',
+  #     'HOST' : 'localhost',
+  #     'PORT': '5432'
+  # }
+#}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 
 
 # Password validation
