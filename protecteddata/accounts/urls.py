fb1964c6fe50b95487b16b8a532b6b2539027160
YourBunnyWrote0000 <57093177+YourBunnyWrote0000@users.noreply.github.com>
2021-06-08 16:58:41 +0300

    Customizing password functions

--- protecteddata/accounts/urls.py
+++ protecteddata/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import SignUpView, LoginView
+from .views import SignUpView, LoginView, PassResetView
 
 
 urlpatterns = [
     path('signup/', SignUpView, name='signup'),
-    path('login/', LoginView, name='login')
+    path('login/', LoginView, name='login'),
+    path('password_reset/', PassResetView, name = 'passresetview'),
 ]
--- protecteddata/accounts/views.py
+++ protecteddata/accounts/views.py
@@ -1,8 +1,16 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import BadHeaderError, send_mail
+from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
 from .forms import SignUpForm
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
 from django.contrib import messages
 
 
@@ -35,3 +43,35 @@ def LoginView(request):
     else:
         form = AuthenticationForm()
     return render(request, 'registration/login.html', context={'form': form})
+
+def PassResetView(request):
+    if request.method == 'POST':
+        form = PasswordResetForm(request.POST)
+        if form.is_valid():
+            data = request.POST['email']
+            finded_users = User.objects.filter(Q(email = data))
+            if finded_users.exists():
+                for user in finded_users:
+                    subject = "Password reset"
+                    email_template_name = 'registration/pass_reset_email.txt'
+                    c = {
+                        'email': user.email,
+                        'domain': 'localhost',
+                        'site_name': 'Protected Data',
+                        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                        'user': user,
+                        'token': default_token_generator.make_token(user),
+                        'protocol': 'http',
+                    }
+                    email = render_to_string(email_template_name, c)
+                    try:
+                        send_mail(subject, email, 'ProtectedData@gmail.com', [user.email], fail_silently = False)
+                    except BadHeaderError:
+                        return HttpResponse('Invalid header found.')
+                    return render('password_reset_done.html')
+    pass_reset_form = PasswordResetForm()
+    return render(request, 'registration/password_reset_form.html', context={'pass_reset_form': pass_reset_form})
+
+
+
+
